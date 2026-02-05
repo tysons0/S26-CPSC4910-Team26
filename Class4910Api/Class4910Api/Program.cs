a4d9aad5f7bc550175482ede883dee9436162cfb
Armando <armandosallas0213@gmail.com>
2026-02-04 19:55:37 -0500

    Added EbayServices to api
    Specifically added getting the oauth token used for the ebay api.
    CatallogControllers.cs is just a testing file for the ebay endpoint

--- Class4910Api/Class4910Api/Program.cs
+++ Class4910Api/Class4910Api/Program.cs
@@ -2,8 +2,11 @@ using Class4910Api.Configuration;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddHttpClient<EbayService>();
+
 builder = Startup.CreateBuilder(builder);
 
 WebApplication app = Startup.ConfigureApp(builder);
+
 
 app.Run();
