7564f1f0c9c01167a5dc64a716c2f8efc6047bb0
Derek Smith <dereksmith32526@gmail.com>
2026-02-10 16:48:09 -0500

    Add starter Sponsor Service

---
+++ Class4910Api/Class4910Api/Services/Interfaces/ISponsorService.cs
@@ -0,0 +1,9 @@
+﻿using Class4910Api.Models;
+
+namespace Class4910Api.Services.Interfaces;
+
+public interface ISponsorService
+{
+    Task<Sponsor?> GetSponsorBySponsorId(int sponsorId);
+    Task<Sponsor?> GetSponsorByUserId(int userId);
+}
---
+++ Class4910Api/Class4910Api/Services/SponsorService.cs
@@ -0,0 +1,48 @@
+﻿using System.Data.Common;
+using Class4910Api.Configuration;
+using Class4910Api.Models;
+using Class4910Api.Services.Interfaces;
+using Microsoft.Extensions.Options;
+using System.Data;
+using Microsoft.AspNetCore.Identity;
+using MySql.Data.MySqlClient;
+
+using static Class4910Api.Configuration.ConstantValues;
+
+
+namespace Class4910Api.Services;
+
+public class SponsorService : ISponsorService
+{
+    private readonly ILogger<SponsorService> _logger;
+    private readonly string _dbConnection;
+    private readonly IUserService _userService;
+
+    public SponsorService(ILogger<SponsorService> logger, IOptions<DatabaseConnection> databaseConnection, IUserService userService)
+    {
+        _logger = logger;
+        _dbConnection = databaseConnection.Value.Connection;
+        _userService = userService;
+    }
+
+    public Task<Sponsor?> GetSponsorByName(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Sponsor?> GetSponsorBySponsorId(int sponsorId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Sponsor?> GetSponsorByUserId(int userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    private async Task<User> GetSponsorFromReader(DbDataReader reader)
+    {
+        throw new NotImplementedException();
+        int sponsorId = reader.GetInt32(SponsorIdField.Name);
+    }
+}
