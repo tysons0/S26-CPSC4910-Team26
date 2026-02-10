550dd641bf98e2f13d8df9522f2eb3c228ccaa93
Derek Smith <dereksmith32526@gmail.com>
2026-02-10 16:48:31 -0500

    Add Driver Service

---
+++ Class4910Api/Class4910Api/Services/DriverService.cs
@@ -0,0 +1,35 @@
+﻿using Class4910Api.Configuration;
+using Class4910Api.Models;
+using Class4910Api.Services.Interfaces;
+using Microsoft.Extensions.Options;
+
+using static Class4910Api.Configuration.ConstantValues;
+
+namespace Class4910Api.Services;
+
+public class DriverService : IDriverService
+{
+    private readonly ILogger<DriverService> _logger;
+    private readonly string _dbConnection;
+
+    public DriverService(ILogger<DriverService> logger, IOptions<DatabaseConnection> databaseConnection)
+    {
+        _logger = logger;
+        _dbConnection = databaseConnection.Value.Connection;
+    }
+
+    public Task<Driver?> GetDriverByDriverId(int driverId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Driver?> GetDriverByName(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Driver?> GetDriverByUserId(int userId)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Class4910Api/Class4910Api/Services/Interfaces/IDriverService.cs
@@ -0,0 +1,9 @@
+﻿using Class4910Api.Models;
+
+namespace Class4910Api.Services.Interfaces;
+
+public interface IDriverService
+{
+    Task<Driver?> GetDriverByDriverId(int driverId);
+    Task<Driver?> GetDriverByUserId(int userId);
+}
