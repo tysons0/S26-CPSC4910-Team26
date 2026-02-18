7e8d8754cdee1b96eaf7eeca73fda1c4ce94d2c8
Derek Smith <dereksmith32526@gmail.com>
2026-02-18 18:43:48 -0500

    Add Request Models for endpoint requests

---
+++ Class4910Api/Class4910Api/Models/Requests/PasswordChangeRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Class4910Api.Models.Requests;
+
+public class PasswordChangeRequest
+{
+    public required string UserName { get; init; }
+    public required string CurrentPassword { get; init; }
+    public required string NewPassword { get; init; }
+}
---
+++ Class4910Api/Class4910Api/Models/Requests/UserUpdateRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Class4910Api.Models.Requests;
+
+public class UserUpdateRequest
+{
+    public string? FirstName { get; init; }
+    public string? LastName { get; init; }
+    public string? Email { get; init; }
+    public string? PhoneNumber { get; init; }
+    public string? TimeZone { get; init; }
+    public string? Country { get; init; }
+}
