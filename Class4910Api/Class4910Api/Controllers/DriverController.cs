dc4f01f0cfcdbd3abd1b05afd8149d591faed49e
Derek Smith <dereksmith32526@gmail.com>
2026-02-18 18:46:44 -0500

    Add controllers for user updating as well as controllers needed in the future

---
+++ Class4910Api/Class4910Api/Controllers/DriverController.cs
@@ -0,0 +1,71 @@
+﻿using Class4910Api.Models;
+using Class4910Api.Models.Requests;
+using Class4910Api.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Class4910Api.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("[controller]")]
+public class DriverController : ControllerBase
+{
+
+    private readonly ILogger<DriverController> _logger;
+    private readonly IContextService _contextService;
+    private readonly IDriverService _driverService;
+
+    public DriverController(ILogger<DriverController> logger, IDriverService driverService, IContextService contextService)
+    {
+        _logger = logger;
+        _contextService = contextService;
+        _driverService = driverService;
+    }
+
+    [HttpGet("{userId:int}")]
+    public async Task<ActionResult<Driver>> GetDriver(int userId)
+    {
+        UserRole role = _contextService.GetUserRole(HttpContext);
+        int contextUserId = _contextService.GetUserId(HttpContext);
+
+        Driver? driver = await _driverService.GetDriverByUserId(userId);
+
+        if (driver is null)
+        {
+            return NotFound($"No driver found with ID {userId}");
+        }
+
+#warning Forbid Sponsor as well (If Not in Same Org)
+        if (driver.UserData.Id != contextUserId && role == UserRole.Driver)
+        {
+            return Forbid($"You do not have permission to access driver with ID {contextUserId}");
+        }
+
+        return Ok(driver);
+    }
+
+    [HttpPost("{driverId:int}/address")]
+    public async Task<ActionResult> AddDriverAddress(int driverId, [FromBody] AddressRequest addressRequest)
+    {
+        return Ok("");
+    }
+
+    [HttpPost("{driverId:int}/address/{addressId:int}")]
+    public async Task<ActionResult> ChangePrimaryAddress(int driverId, int addressId)
+    {
+        return Ok("");
+    }
+
+    [HttpPut("{driverId:int}/address/{addressId:int}")]
+    public async Task<ActionResult> UpdateAddress(int driverId, int addressId, [FromBody] AddressRequest addressRequest)
+    {
+        return Ok("");
+    }
+
+    [HttpDelete("{driverId:int}/address")]
+    public async Task<ActionResult> DeleteDriverAddress(int driverId, [FromBody] AddressRequest addressRequest)
+    {
+        return Ok("");
+    }
+}
---
+++ Class4910Api/Class4910Api/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using Class4910Api.Models;
+using Class4910Api.Models.Requests;
+using Class4910Api.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Class4910Api.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly ILogger<UserController> _logger;
+    private readonly IContextService _contextService;
+    private readonly IUserService _userService;
+
+    public UserController(ILogger<UserController> logger, IUserService userService, IContextService contextService)
+    {
+        _logger = logger;
+        _contextService = contextService;
+        _userService = userService;
+    }
+
+
+    [HttpPut("{updateUserId:int}")]
+    public async Task<ActionResult<UserRead>> UpdateUser(int updateUserId, [FromBody] UserUpdateRequest userRequest)
+    {
+        int contextUserId = _contextService.GetUserId(HttpContext);
+        User? userBeingUpdated = await _userService.FindUserById(updateUserId);
+
+        _logger.LogInformation("Received request to update user with id {UserId} from user with id {RequestingUserId}", updateUserId, contextUserId);
+
+        if (updateUserId != contextUserId || userBeingUpdated is null)
+        {
+            return Forbid();
+        }
+
+        _logger.LogInformation("Updating user with id {UserId}", contextUserId);
+
+        User? updatedUser = await _userService.UpdateUser(updateUserId, userRequest);
+
+        if (updatedUser == null)
+        {
+            return BadRequest("Error Updating User");
+        }
+
+        _logger.LogInformation("Successfully updated user with id {UserId}", contextUserId);
+        return Ok(updatedUser.ToReadFormat());
+    }
+}
