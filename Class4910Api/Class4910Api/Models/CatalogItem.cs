466e74a400a241edb107db01a01db8f9c84746cc
Asallas <armandosallas0213@gmail.com>
2026-02-19 14:29:18 -0500

    Quick syntax error fix

--- Class4910Api/Class4910Api/Models/CatalogItem.cs
+++ Class4910Api/Class4910Api/Models/CatalogItem.cs
@@ -2,11 +2,11 @@
 
 public class CatalogItem
 {
-    public int CatalogItemID { get; set;
+    public int CatalogItemID { get; set; }
     public string EbayItemId { get; set; } = null!;
     public string Title { get; set; } = null!;
     public string? ImageUrl { get; set; }
-    public int Points { get; set; 
+    public int Points { get; set; }
     public bool IsActive { get; set; }
 }
 
