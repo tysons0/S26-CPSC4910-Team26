540dcbf66103d4639aaf1d9dc8bfa5f611ea658c
Derek Smith <dereksmith32526@gmail.com>
2026-03-01 01:39:09 -0500

    Finish Adding Tests

--- Class4910Api/Class4910Tests/Integration/AuthController/AuthControllerTests.cs
+++ Class4910Api/Class4910Tests/Integration/AuthController/AuthControllerTests.cs
@@ -172,8 +172,6 @@ public class AuthControllerTests : IClassFixture<Class4910ApiFactory>
         Assert.Null(userData);
     }
 
-#warning Add GetTokenInfo 
-
     [Theory]
     [InlineData(seedAdminUserName, seedAdminPassword)]
     [InlineData(seedDriverUserName, seedDriverPassword)]
@@ -193,18 +191,172 @@ public class AuthControllerTests : IClassFixture<Class4910ApiFactory>
         LoginResult? result = await GetClassFromResponse<LoginResult>(loginResponse);
         client = AttachTokenToClient(client, result?.Token);
 
-        HttpResponseMessage response = await client.GetAsync(Routes.Auth.MeTokenInfo, _cancelToken);
+        HttpResponseMessage response = await client.GetAsync(Routes.Auth.MeTokenInfoFull, _cancelToken);
         TokenInfo? tokenInfo = await GetClassFromResponse<TokenInfo>(response);
 
         // Assert
         Assert.Equal(expectedResponseCode, response.StatusCode);
         Assert.NotNull(tokenInfo);
-#warning Add more assertions to validate the token info properties
     }
-#warning Add ChangePassword
-#warning Add RegisterAdmin
-#warning Add RegisterDriver
-#warning Add RegisterSponsor
+
+    [Theory]
+    [InlineData(seedAdminUserName, seedAdminPassword, "newAdminPassword")]
+    [InlineData(seedDriverUserName, seedDriverPassword, "newDriverPassword")]
+    [InlineData(seedSponsorUserName, seedSponsorPassword, "newSponsorPassword")]
+    public async Task ChangePassword_IsSuccessful_WithValidInformation(string username, string currentPassword, string newPassword)
+    {
+        // Arrange
+        HttpStatusCode expectedResponseCode = HttpStatusCode.OK;
+        HttpClient client = _apiFactory.CreateClient();
+        UserRequest beforeChangeLoginRequest = new()
+        {
+            UserName = username,
+            Password = currentPassword
+        };
+        UserRequest afterChangeLoginRequest = new() 
+        { 
+            UserName = username,
+            Password = newPassword 
+        };
+        PasswordChangeRequest passwordChangeRequest = new()
+        {
+            UserName = username,
+            CurrentPassword = currentPassword,
+            NewPassword = newPassword,
+        };
+        PasswordChangeRequest passwordChangeBackRequest = new() 
+        { 
+            UserName = username,
+            CurrentPassword = newPassword,
+            NewPassword = currentPassword
+        };
+
+        // Act
+        HttpResponseMessage loginResponse = await client.PostAsJsonAsync(Routes.Auth.LoginFull, beforeChangeLoginRequest, _cancelToken);
+        LoginResult? loginResult = await GetClassFromResponse<LoginResult>(loginResponse);
+        client = AttachTokenToClient(client, loginResult?.Token);
+
+        HttpResponseMessage passwordChangeResponse = await client.PostAsJsonAsync(Routes.Auth.PasswordChangeFull, passwordChangeRequest, _cancelToken);
+
+        HttpResponseMessage loginResponseAfter = await client.PostAsJsonAsync(Routes.Auth.LoginFull, afterChangeLoginRequest, _cancelToken);
+        LoginResult? loginResultAfter = await GetClassFromResponse<LoginResult>(loginResponseAfter);
+        client = AttachTokenToClient(client, loginResultAfter?.Token);
+
+        HttpResponseMessage passwordChangeBackResponse = await client.PostAsJsonAsync(Routes.Auth.PasswordChangeFull, passwordChangeBackRequest, _cancelToken);
+
+        HttpResponseMessage loginResponseAfterChangeBack = await client.PostAsJsonAsync(Routes.Auth.LoginFull, beforeChangeLoginRequest, _cancelToken);
+        LoginResult? loginResultAfterChangeBack = await GetClassFromResponse<LoginResult>(loginResponseAfterChangeBack);
+
+        // Assert
+        Assert.Equal(expectedResponseCode, loginResponse.StatusCode);
+        Assert.Equal(expectedResponseCode, passwordChangeResponse.StatusCode);
+        Assert.Equal(expectedResponseCode, loginResponseAfter.StatusCode);
+        Assert.Equal(expectedResponseCode, passwordChangeBackResponse.StatusCode);
+        Assert.Equal(expectedResponseCode, loginResponseAfterChangeBack.StatusCode);
+
+        Assert.NotNull(loginResult);
+        Assert.NotNull(loginResultAfter);
+        Assert.NotNull(loginResultAfterChangeBack);
+
+        Assert.NotNull(loginResult.User);
+        Assert.NotNull(loginResultAfter.User);
+        Assert.NotNull(loginResultAfterChangeBack.User);
+
+        Assert.Null(loginResult.Error);
+        Assert.Null(loginResultAfter.Error);
+        Assert.Null(loginResultAfterChangeBack.Error);
+
+        Assert.Equal(loginResult.User.Username, username);
+        Assert.Equal(loginResultAfter.User.Username, username);
+        Assert.Equal(loginResultAfterChangeBack.User.Username, username);
+    }
+
+
+    [Theory]
+    [InlineData("testAdmin1", "testAdminPw1")]
+    [InlineData("testAdmin2", "SpecialCharacterstestAdminPw1{]}!)")]
+    [InlineData("testAdmin3", "LongPasswordTesttestAdminPw1testAdminPw1testAdminPw1testAdminPw1testAdminPw1testAdminPw1testAdminPw1testAdminPw1testAdminPw1")]
+    [InlineData("LongUsernametestAdmin4UsernametestAdmin49", "testAdminPw")]
+    [InlineData("SpecialCahracterstestAdmin5[]!SAIJ@#8", "testAdminPw")]
+    [InlineData("testAdmin69837120", "testAdminPw1")]
+    [InlineData("LongUsernametestAdmin4UsernametestAdmin43", "SpecialCharacterstestAdminPw1{]}!)")]
+    [InlineData("SpecialCahracterstestAdmin5[]!SAIJ@#23", "SpecialCharacterstestAdminPw1{]}!)")]
+    [InlineData("LongUsernametestAdmin4UsernametestAdmin41", "LongPasswordTesttestAdminPw1testAdminPw1testAdminPw1testAdminPw1testAdminPw1testAdminPw1testAdminPw1testAdminPw1testAdminPw1")]
+    [InlineData("SpecialCahracterstestAdmin5[]!SAIJ@#2", "LongPasswordTesttestAdminPw1testAdminPw1testAdminPw1testAdminPw1testAdminPw1testAdminPw1testAdminPw1testAdminPw1testAdminPw1")]
+    public async Task RegisterAdmin_IsSuccessful_WithValidInputAndToken(string username, string password)
+    {
+        // Arrange
+        HttpStatusCode expectedRegisterResponseCode = HttpStatusCode.Created;
+        HttpStatusCode expectedLoginResponseCode = HttpStatusCode.OK;
+        HttpClient client = _apiFactory.CreateClient();
+        UserRequest userRequest = new()
+        {
+            UserName = username,
+            Password = password
+        };
+
+        // Act
+        HttpResponseMessage seedAdminloginResponse = await client.PostAsJsonAsync(Routes.Auth.LoginFull, seedAdminRequest, _cancelToken);
+        LoginResult? seedAdminloginResult = await GetClassFromResponse<LoginResult>(seedAdminloginResponse);
+        client = AttachTokenToClient(client, seedAdminloginResult?.Token);
+
+        HttpResponseMessage registerResponse = await client.PostAsJsonAsync(Routes.Auth.RegisterAdminFull, userRequest, _cancelToken);
+
+        HttpResponseMessage loginResponse = await client.PostAsJsonAsync(Routes.Auth.LoginFull, userRequest, _cancelToken);
+        LoginResult? loginResult = await GetClassFromResponse<LoginResult>(loginResponse);
+
+        // Assert
+        Assert.Equal(expectedRegisterResponseCode, registerResponse.StatusCode);
+        Assert.Equal(expectedLoginResponseCode, loginResponse.StatusCode);
+
+        Assert.NotNull(loginResult);
+        Assert.NotNull(loginResult.User);
+
+        Assert.Null(loginResult.Error);
+
+        Assert.Equal(loginResult.User.Username, username);
+    }
+
+    [Theory]
+    [InlineData("testDriver1", "testDriverPw1")]
+    [InlineData("testDriver2", "SpecialCharacterstestDriverPw1{]}!)")]
+    [InlineData("testDriver3", "LongPasswordTesttestDriverPw1testDriverPw1testDriverPw1testDriverPw1testDriverPw1testDriverPw1testDriverPw1testDriverPw1testDriverPw1")]
+    [InlineData("LongUsernametestDriver4UsernametestDriver4", "testDriverPw")]
+    [InlineData("SpecialCahracterstestDriver5[]!SAIJ@#", "testDriverPw")]
+    [InlineData("testDriver6983712", "testDriverPw1")]
+    [InlineData("LongUsernametestDriver4UsernametestDriver42", "SpecialCharacterstestDriverPw1{]}!)")]
+    [InlineData("SpecialCahracterstestDriver5[]!SAIJ@#3", "SpecialCharacterstestDriverPw1{]}!)")]
+    [InlineData("LongUsernametestDriver4UsernametestDriver49", "LongPasswordTesttestDriverPw1testDriverPw1testDriverPw1testDriverPw1testDriverPw1testDriverPw1testDriverPw1testDriverPw1testDriverPw1")]
+    [InlineData("SpecialCahracterstestDriver5[]!SAIJ@#8", "LongPasswordTesttestDriverPw1testDriverPw1testDriverPw1testDriverPw1testDriverPw1testDriverPw1testDriverPw1testDriverPw1testDriverPw1")]
+    public async Task RegisterDriver_IsSuccessful_WithValidInput(string username, string password)
+    {
+        // Arrange
+        HttpStatusCode expectedRegisterResponseCode = HttpStatusCode.Created;
+        HttpStatusCode expectedLoginResponseCode = HttpStatusCode.OK;
+        HttpClient client = _apiFactory.CreateClient();
+        UserRequest userRequest = new()
+        {
+            UserName = username,
+            Password = password
+        };
+
+        // Act
+        HttpResponseMessage registerResponse = await client.PostAsJsonAsync(Routes.Auth.RegisterDriverFull, userRequest, _cancelToken);
+
+        HttpResponseMessage loginResponse = await client.PostAsJsonAsync(Routes.Auth.LoginFull, userRequest, _cancelToken);
+        LoginResult? loginResult = await GetClassFromResponse<LoginResult>(loginResponse);
+
+        // Assert
+        Assert.Equal(expectedRegisterResponseCode, registerResponse.StatusCode);
+        Assert.Equal(expectedLoginResponseCode, loginResponse.StatusCode);
+
+        Assert.NotNull(loginResult);
+        Assert.NotNull(loginResult.User);
+
+        Assert.Null(loginResult.Error);
+
+        Assert.Equal(loginResult.User.Username, username);
+    }
 
     private async Task<T?> GetClassFromResponse<T>(HttpResponseMessage response) where T : class
     {
