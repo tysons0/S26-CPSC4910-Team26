4075da8faf7d904a79952dbdf4703a393d7dce0b
Derek Smith <dereksmith32526@gmail.com>
2026-03-05 00:15:07 -0500

    Change password change request to not require username

--- Class4910Api/Class4910Api/Models/Requests/PasswordChangeRequest.cs
+++ Class4910Api/Class4910Api/Models/Requests/PasswordChangeRequest.cs
@@ -2,7 +2,6 @@
 
 public class PasswordChangeRequest
 {
-    public required string UserName { get; init; }
     public required string CurrentPassword { get; init; }
     public required string NewPassword { get; init; }
 }
