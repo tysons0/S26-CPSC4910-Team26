5a3d5036d7228891d78e1b859347bd73fd2a7e40
Derek Smith <dereksmith32526@gmail.com>
2026-03-12 11:55:52 -0400

    Actually fix routing, didn't see that controller wasn't added to it.

--- Class4910Api/Class4910Api/Controllers/EmailController.cs
+++ Class4910Api/Class4910Api/Controllers/EmailController.cs
@@ -9,7 +9,7 @@ namespace Class4910Api.Controllers
 
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     public class EmailController : ControllerBase
     {
         private readonly IEmailService _emailService;
@@ -39,7 +39,7 @@ namespace Class4910Api.Controllers
 
             _resetTokens[token] = (request.Email, expiry);
 
-            string resetLink = $"https://team26api.cpsc4911.com/reset-password?token={token}&username={Uri.EscapeDataString(user.Username)}";
+            string resetLink = $"https://team26api.cpsc4911.com/Email/reset-password?token={token}&username={Uri.EscapeDataString(user.Username)}";
 
             await _emailService.SendEmailAsync(
                 request.Email,
